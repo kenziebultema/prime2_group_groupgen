19a2902451d6b08db5ba8483d564cc6aeae19574
Kenzie Bultema <krbultema@gmail.com>
2016-03-23 15:40:01 -0500

    shuffle works, styling from joette

--- server/public/assets/scripts/client.js
+++ server/public/assets/scripts/client.js
@@ -1,12 +1,27 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope){
+myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
+
+    $scope.shuffle = function(array){
+        console.log('shuffled');
+        for (var i = array.length - 1; i > 0; i--) {
+           var j = Math.floor(Math.random() * (i + 1));
+           var temp = array[i];
+           array[i] = array[j];
+           array[j] = temp;
+        }
+           return array;
+    }
 
     $scope.select = function(number){
         console.log('clicked', number);
+
+        $http.get('/assets/data/students.json').then(function(response){
+            $scope.students = response.data.students;
+            $scope.shuffle($scope.students);
+        });
     }
 
-    $scope.shuffle = function(){
-        console.log('shuffled');
-    }
 }]);
+
+console.log('connected');
